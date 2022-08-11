65ca4f1e029ebc3c05b91b494bce7045e3194b1b
Diana Jurca <diana.roxana.lazar@gmail.com>
2022-08-11 18:34:09 +0200

    last touches

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//circle logo is home link
 function home() {
   window.location.href = "/index.html";
 }
+
+//Get in touch takes user to Contact page
+const mainBtn = document.querySelector(".btn-main");
+mainBtn.addEventListener('click', function (e) {
+  window.location.href= "/contact.html"
+});
